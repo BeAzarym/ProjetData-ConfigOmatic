bde26aa77c6c07931a499e3ba8303ef03126179c
BeAzarym <58728526+BeAzarym@users.noreply.github.com>
2020-04-06 19:57:52 +0200

    Add files via upload

--- ConfigOmatic-TABLE.sql
+++ ConfigOmatic-TABLE.sql
@@ -11,8 +11,57 @@ CREATE TABLE tbConfigSave(
     "UserId" INTEGER NOT NULL DEFAULT AUTOINCREMENT, 
     "ConfigId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
     
-    CONSTRAINT "pkConfigSave" PRIMARY KEY ("UserId" ASC)
+    CONSTRAINT "pkConfigSave" PRIMARY KEY ("ConfigId" ASC),
     CONSTRAINT "fk__tbConfigSave__tbUser" FOREIGN KEY (UserId) REFERENCES tbUser (UserId)
+)
+
+CREATE TABLE tbCarteMere(
+    "CarteMereId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "CarteMereName" long varchar NOT NULL,
+    "CarteMerePrix" DPrix,
+
+    CONSTRAINT "pkCarteMere" PRIMARY KEY ("CarteMereId" ASC),
+)
+
+CREATE TABLE tbCarteGraphique(
+    "CarteGraphiqueId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "CarteGraphiqueName" long varchar NOT NULL,
+    "CarteGraphiquePrix" DPrix,
+
+    CONSTRAINT "pkCarteGraphique" PRIMARY KEY ("CartegraphiqueId" ASC),
+)
+
+CREATE TABLE tbProcesseur(
+    "ProcesseurId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "ProcesseurName" long varchar NOT NULL,
+    "ProcesseurPrix" DPrix,
+    
+    CONSTRAINT "pkProcesseur" PRIMARY KEY ("ProcesseurId" ASC),
+)
+
+CREATE TABLE tbBoitier(
+    "BoitierId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "BoitierName" long varchar NOT NULL,
+    "BoitierPrix" DPrix,
+
+    CONSTRAINT "pkBoitier" PRIMARY KEY ("BoitierId" ASC),
+)
+
+CREATE TABLE tbRAM(
+    "RamId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "RamName" long varchar NOT NULL,
+    "RamPrix" DPrix,
+    
+    CONSTRAINT "pkRAM" PRIMARY KEY ("RamId" ASC),
+
+)
+
+CREATE TABLE tbDisqueDur(
+    "DisqueDurId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "DisqueDurName" long varchar NOT NULL,
+    "DisqueDurPrix" DPrix,
+
+    CONSTRAINT "pkDisqueDur" PRIMARY KEY ("DisqueDurId" ASC),
 )
 
 CREATE TABLE tbConfig(
@@ -24,62 +73,13 @@ CREATE TABLE tbConfig(
     "BoitierId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
     "RamId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
 
-    CONSTRAINT "pkConfig" PRIMARY KEY ("ConfigId" ASC)
-    CONSTRAINT "fk__tbConfig__tbConfigSave" FOREIGN KEY (ConfigId) REFERENCES tbConfigSave (UserId)
+  
+    CONSTRAINT "pkConfig" PRIMARY KEY ("ConfigId" ASC),
+    CONSTRAINT "fk__tbConfig__tbConfigSave" FOREIGN KEY (ConfigId) REFERENCES tbConfigSave (ConfigId),
+    CONSTRAINT "fk__tbConfig__tbProcesseur" FOREIGN KEY (ProcesseurId) REFERENCES tbProcesseur (ProcesseurId),
+    CONSTRAINT "fk__tbConfig__tbCarteMere" FOREIGN KEY (CarteMereId) REFERENCES tbCarteMere (CarteMereId),
+    CONSTRAINT "fk__tbConfig__tbDisqueDur" FOREIGN KEY (DisqueDurId) REFERENCES tbDisqueDur (DisqueDurId), 
+    CONSTRAINT "fk__tbConfig__tbCarteGraphique" FOREIGN KEY (CarteGraphiqueId) REFERENCES tbCarteGraphique (CarteGraphiqueId),
+    CONSTRAINT "fk__tbConfig__tbBoitier" FOREIGN KEY (BoitierId) REFERENCES tbBoitier (BoitierId), 
+    CONSTRAINT "fk__tbConfig__tbRAM" FOREIGN KEY (RamId) REFERENCES tbRAM (RamId)
 )
-
-CREATE TABLE tbCarteMere(
-    "CarteMereId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "CarteMereName" long varchar NOT NULL,
-    "CarteMerePrix" DPrix,
-
-    CONSTRAINT "pkCarteMere" PRIMARY KEY ("CarteMereId" ASC)
-    CONSTRAINT "fk__tbCarteMere__tbConfig" FOREIGN KEY (CarteMereId) REFERENCES tbConfig (CarteMereId)
-    
-)
-
-CREATE TABLE tbCarteGraphique(
-    "CarteGraphiqueId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "CarteGraphiqueName" long varchar NOT NULL,
-    "CarteGraphiquePrix" DPrix,
-
-    CONSTRAINT "pkCarteGraphique" PRIMARY KEY ("CartegraphiqueId" ASC)
-    CONSTRAINT "fk__tbCarteGraphique__tbConfig" FOREIGN KEY (CarteGraphiqueId) REFERENCES tbConfig (CarteGraphiqueId)
-)
-
-CREATE TABLE tbProcesseur(
-    "ProcesseurId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "ProcesseurName" long varchar NOT NULL,
-    "ProcesseurPrix" DPrix,
-    
-    CONSTRAINT "pkProcesseur" PRIMARY KEY ("ProcesseurId" ASC)
-    CONSTRAINT "fk__tbProcesseur__tbConfig" FOREIGN KEY (ProcesseurId) REFERENCES tbConfig (ProcesseurId)
-)
-
-CREATE TABLE tbBoitier(
-    "BoitierId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "BoitierName" long varchar NOT NULL,
-    "BoitierPrix" DPrix,
-
-    CONSTRAINT "pkBoitier" PRIMARY KEY ("BoitierId" ASC)
-    CONSTRAINT "fk__tbBoitier__tbConfig" FOREIGN KEY (BoitierId) REFERENCES tbConfig (BoitierId)
-)
-
-CREATE TABLE tbRAM(
-    "RamId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "RamName" long varchar NOT NULL,
-    "RamPrix" DPrix,
-    
-    CONSTRAINT "pkRAM" PRIMARY KEY ("RamId" ASC)
-    CONSTRAINT "fk__tbRAM__tbConfig" FOREIGN KEY (RamId) REFERENCES tbConfig (RamId)
-)
-
-CREATE TABLE tbDisqueDur(
-    "DisqueDurId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "DisqueDurName" long varchar NOT NULL,
-    "DisqueDurPrix" DPrix,
-
-    CONSTRAINT "pkDisqueDur" PRIMARY KEY ("DisqueDurId" ASC)
-    CONSTRAINT "fk__tbDisqueDur__tbConfig" FOREIGN KEY (DisqueDurId) REFERENCES tbConfig (DisqueDurId)
-)
-
