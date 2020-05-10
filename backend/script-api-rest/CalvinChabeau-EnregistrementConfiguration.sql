2971510d91bccabd194f2e0b34a76a3284ad38ee
BeAzarym <58728526+BeAzarym@users.noreply.github.com>
2020-05-10 23:30:13 +0200

    Add files via upload

--- backend/script-api-rest/CalvinChabeau-EnregistrementConfiguration.sql
+++ backend/script-api-rest/CalvinChabeau-EnregistrementConfiguration.sql
@@ -1,6 +1,26 @@
 
 /* auteur : CHABEAU CALVIN HE221842*/
-ALTER PROCEDURE "DBA"."enregistrer_config"(@pseudo varchar(50),@Processeur int , @Boitier INT, @Ram int , @CarteGraphique int , @CarteMere int, @DisqueDur int )
+
+CREATE FUNCTION "DBA"."RecupConfigId"( in @pseudo varchar(50)  )
+RETURNS INTEGER
+NOT DETERMINISTIC
+BEGIN
+	DECLARE configIdRecup INTEGER;
+	--
+    set configIdRecup = (select ConfigId from tbConfig where DBA.tbConfig.pseudo = @pseudo);
+	--
+    
+    RETURN configIdRecup;
+END
+**************************************
+CREATE PROCEDURE "DBA"."UpdateConfigId"( @pseudo varchar(50) )
+BEGIN
+     UPDATE tbUser
+     set DBA.tbUser.ConfigId = (select RecupConfigId(@pseudo))
+     where pseudo = @pseudo   
+END
+**************************************
+CREATE PROCEDURE "DBA"."enregistrer_config"(@pseudo varchar(50),@Processeur int , @Boitier INT, @Ram int , @CarteGraphique int , @CarteMere int, @DisqueDur int )
  
 BEGIN
    
@@ -14,3 +34,4 @@ END
 ***************************************
 
 CREATE SERVICE "enregistre_config" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call enregistrer_Config(:Processeur,:Boitier,:Ram,:CarteGraphique,:CarteMere,:DisqueDur);
+
