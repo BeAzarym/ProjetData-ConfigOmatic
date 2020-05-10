3a61b290037376037ac1570ae2813ed7a1efaf3d
BeAzarym <58728526+BeAzarym@users.noreply.github.com>
2020-05-10 23:19:09 +0200

    Add files via upload

--- backend/script-api-rest/CalvinChabeau-EnregistrementConfiguration.sql
+++ backend/script-api-rest/CalvinChabeau-EnregistrementConfiguration.sql
@@ -1,17 +1,16 @@
 
 /* auteur : CHABEAU CALVIN HE221842*/
-
-ALTER PROCEDURE "DBA"."enregistrer_config"(@Processeur int , @Boitier INT, @Ram int , @CarteGraphique int , @CarteMere int, @DisqueDur int )
+ALTER PROCEDURE "DBA"."enregistrer_config"(@pseudo varchar(50),@Processeur int , @Boitier INT, @Ram int , @CarteGraphique int , @CarteMere int, @DisqueDur int )
  
 BEGIN
-    
+   
    insert into DBA.tbConfig
-    (ProcesseurId , BoitierId , RamId  , CarteGraphiqueId  , CarteMereId , DisqueDurId)
+    (ProcesseurId , BoitierId , RamId  , CarteGraphiqueId  , CarteMereId , DisqueDurId,pseudo)
     values
-    (@Processeur , @Boitier , @Ram  , @CarteGraphique , @CarteMere, @DisqueDur)     
+    (@Processeur , @Boitier , @Ram  , @CarteGraphique , @CarteMere, @DisqueDur, @pseudo);     
 
+    call UpdateConfigId(@pseudo)
 END
-
 ***************************************
 
 CREATE SERVICE "enregistre_config" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call enregistrer_Config(:Processeur,:Boitier,:Ram,:CarteGraphique,:CarteMere,:DisqueDur);
