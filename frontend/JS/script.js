67eefefaf5fc2ab9403371d56085c1db554faded
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-05-10 22:18:03 +0200

    Update script.js

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -12,56 +12,7 @@ function chargement(){
 }
 
 
-//function qui enregistre le pseudo vérifié
-function enregistrePseudo(formulaire) {
-
-    let pseudo =  document.getElementById("pseudo").value
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET','enregistrer_nom?usr=' + pseudo,true);
-    xhr.send();
-    return false;
-
-}
-
-
-//verification du pseudo dand la db
-function traiterPseudo(){
-
-    let pseudo =  document.getElementById("pseudo").value
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET','check_pseudo?usr=' + pseudo ,true);
-    xhr.onload = verifePseudo;
-    xhr.send();
-
-    return false;
-
-}
-
-//conditions de la verification du pseudo avec deux possibilités
-//enregistre le pseudo ou demande d introduire un autre pseudo et de passer a la suite
-
-function verifePseudo(){
-
-    let pseudo =  document.getElementById("pseudo").value
-
-    let reponse = this.response;
-
-    console.log(reponse)
-    if( reponse == 1 ){
-        alert("Ce pseudo existe deja , vous etes bien  : " + pseudo + " Si non trouvé un autre pseudo")
-
-    }
-
-    if( reponse == 0 ){
-        enregistrePseudo();
-        alert("vous avez bien été enregistré vous pouvez descendre sur la page");
-    }
-    return false;
-}
-
-
+https://github.com/BeAzarym/ProjetData-ConfigOmatic/blob/master/frontend/JS/script.js
 
 
 
