18290b80512369345c364c68eded17f12616f168
BeAzarym <58728526+BeAzarym@users.noreply.github.com>
2020-05-10 19:46:23 +0200

    Add files via upload

---
+++ frontend/JS/CalvinCHABEAU-Enregistrement-configuration.js
@@ -0,0 +1,22 @@
+function enregistrerDonnéeConfiguration () {
+    let Processeur = document.getElementsByName("inputProcesseur")[0].values;
+    let Boitier = document.getElementsByName("inputBoitier")[0].values;
+    let Ram = document.getElementsByName("inputMV")[0].values;
+    let CarteGraphique = document.getElementsByName("inputCG")[0].values;
+    let CarteMere = document.getElementsByName("inputCM")[0].values;
+    let DisqueDur = document.getElementsByName("inputDD")[0].values;
+}
+
+function enregistrerConfiguration () {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET','?' + "enregistre_config?Processeur="+Processeur+"&Boitier="+Boitier+"&Ram="+Ram+"&CarteGraphique="CarteGraphique+"&CarteMere="+CarteMere+"&DisqueDur="+DisqueDur,true);
+    xhr.onload =enregistrerDonnéeConfiguration();
+    xhr.send();
+
+    return false;
+
+}
+
+
+
+
