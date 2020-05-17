f06090a1edaac4bcb2891a0e52b6c1745adca614
delma159 <59198120+delma159@users.noreply.github.com>
2020-05-17 17:03:15 +0200

    Update script.js

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -365,3 +365,42 @@ function displayIdCompBoitier(z) {
     getInput.value = z;
     console.log(getInput.value);
 }
+//fonction permettant de recuperer la config en fonction du pseudo présent sur la page
+function recupConfig(){
+    let nom = document.getElementById("pseudo").value;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'recup_config?pseu=' + nom, true);
+    xhr.onload = function testConfig(){
+        let reponseConfig = this.response;
+        recupComposant(reponseConfig);
+    }
+    xhr.send();
+
+}
+
+//fonction permettant de récuperer les composant lié à l'id de la config
+function recupComposant(reponseConfig){
+    let config = reponseConfig;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'recup_composant?config=' + config, true);
+    xhr.onload = afficherComposant;
+
+    xhr.send();
+}
+
+//affichage de la config sur la page
+function afficherComposant(){
+    document.getElementById("configAffichage").innerHTML = '';
+    let reponseComposant = JSON.parse(this.response);
+    let tableComposant = '<table>';
+    for (let composant of reponseComposant){
+        tableComposant += '<tr><td>Nom de la config :</td><td>' + document.getElementById("pseudo").value + '</td></tr>'
+            +'<tr><td>Boitier :</td><td>' + composant.BoitierName + '</td></tr>'
+            +'<tr><td>Carte Graphique :</td><td>' + composant.CarteGraphiqueName + '</td></tr>'
+            +'<tr><td>Carte Mère :</td><td>' + composant.CarteMereName + '</td></tr>'
+            +'<tr><td>Disque Dur :</td><td>' + composant.DisqueDurName + '</td></tr>'
+            +'<tr><td>Processeur :</td><td>' + composant.ProcesseurName + '</td></tr>'
+            +'<tr><td>Ram :</td><td>' + composant.RamName + '</td></tr></table>'
+    }
+    document.getElementById("configAffichage").innerHTML += tableComposant;
+}
