0077e1de94cff63ab5a0ece8cda5aeda9cdd08f3
delma159 <59198120+delma159@users.noreply.github.com>
2020-05-17 16:46:36 +0200

    Update SalhiAdel-afficherConfig.js

--- frontend/JS/SalhiAdel-afficherConfig.js
+++ frontend/JS/SalhiAdel-afficherConfig.js
@@ -16,9 +16,9 @@
 */
 //fonction permettant de recuperer la config en fonction du pseudo présent sur la page
 function recupConfig(){
-    let pseu = document.getElementById("pseudo").value;
+    let nom = document.getElementById("pseudo").value;
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'recup_config?pseu=' + pseu, true);
+    xhr.open('GET', 'recup_config?pseu=' + nom, true);
     xhr.onload = function testConfig(){
         let reponseConfig = this.response;
         recupComposant(reponseConfig);
@@ -32,23 +32,23 @@ function recupComposant(reponseConfig){
     let config = reponseConfig;
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'recup_composant?config=' + config, true);
-    xhr.onload = testComposant;
+    xhr.onload = afficherComposant;
 
     xhr.send();
 }
 
 //affichage de la config sur la page
-function testComposant(){
+function afficherComposant(){
     let reponseComposant = JSON.parse(this.response);
     let tableComposant = '<table>';
-    for (let i of reponseComposant){
+    for (let composant of reponseComposant){
         tableComposant += '<tr><td>Nom de la config :</td><td>' + document.getElementById("pseudo").value + '</td></tr>'
-            +'<tr><td>Boitier :</td><td>' + i.BoitierName + '</td></tr>'
-            +'<tr><td>Carte Graphique :</td><td>' + i.CarteGraphiqueName + '</td></tr>'
-            +'<tr><td>Carte Mère :</td><td>' + i.CarteMereName + '</td></tr>'
-            +'<tr><td>Disque Dur :</td><td>' + i.DisqueDurName + '</td></tr>'
-            +'<tr><td>Processeur :</td><td>' + i.ProcesseurName + '</td></tr>'
-            +'<tr><td>Ram :</td><td>' + i.RamName + '</td></tr></table>'
+            +'<tr><td>Boitier :</td><td>' + composant.BoitierName + '</td></tr>'
+            +'<tr><td>Carte Graphique :</td><td>' + composant.CarteGraphiqueName + '</td></tr>'
+            +'<tr><td>Carte Mère :</td><td>' + composant.CarteMereName + '</td></tr>'
+            +'<tr><td>Disque Dur :</td><td>' + composant.DisqueDurName + '</td></tr>'
+            +'<tr><td>Processeur :</td><td>' + composant.ProcesseurName + '</td></tr>'
+            +'<tr><td>Ram :</td><td>' + composant.RamName + '</td></tr></table>'
     }
     document.getElementById("configAffichage").innerHTML += tableComposant;
 }
